2785548172e45b1649a2f463d18f1b392430d06d
martinortiz13 <martinortiz13@gmail.com>
2017-07-08 16:12:11 -0600

    modified the .gitingnore file

---
+++ clinic-server/src/main/java/com/clinic/clinicserver/ClinicServerApplication.java
@@ -0,0 +1,12 @@
+package com.clinic.clinicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicServerApplication.class, args);
+	}
+}
