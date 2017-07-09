0ef9b82f90bbcc4a5ac41e319c907443c20dc5b5
osanchez1985 <olvinsanchez1985@hotmail.com>
2017-07-08 19:41:42 -0600

    created entity Patient

---
+++ clinic-server/src/main/java/com/clinic/clinicserver/patients/Patient.java
@@ -0,0 +1,63 @@
+package com.clinic.clinicserver.patients;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Patient {
+
+	private String employeId;
+	private String name;
+	private String lastName;
+	private String address;
+	private String phoneNumber;
+	private Date birthDate;
+	private String gender;
+	
+	public String getEmployeId() {
+		return employeId;
+	}
+	public void setEmployeId(String employeId) {
+		this.employeId = employeId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+	
+}
