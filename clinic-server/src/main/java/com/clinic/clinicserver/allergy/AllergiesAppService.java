38169f7f01ecbe68794ea99123d69734c5d6325c
osanchez1985 <olvinsanchez1985@hotmail.com>
2017-07-15 14:42:46 -0600

    adding a new service to get allergies

--- clinic-server/src/main/java/com/clinic/clinicserver/allergy/AllergiesAppService.java
+++ clinic-server/src/main/java/com/clinic/clinicserver/allergy/AllergiesAppService.java
@@ -1,8 +1,42 @@
 package com.clinic.clinicserver.allergy;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AllergiesAppService {
+	
+	public AllergiesAppService(AllergiesRepository allergiesRepository) {
+		super();
+		this.allergiesRepository = allergiesRepository;
+	}
 
+
+	@Autowired
+	private final AllergiesRepository allergiesRepository;
+	
+	
+ public List<AllergiesDto> getAllergies(){
+	 
+	 Iterable<Allergies> allergies = allergiesRepository.findAll();
+      
+	 List<AllergiesDto> allergiesDto = new ArrayList<AllergiesDto>();
+	 for(Allergies allergy :allergies)
+	 {
+		 AllergiesDto item = new AllergiesDto();
+		 item.setAllergyId(allergy.getAllergyId());
+		 item.setAllergyName(allergy.getAllergyName());
+		 item.setDescription(allergy.getDescription());
+		 allergiesDto.add(item);
+	 }
+	 
+	 return allergiesDto;
+
+			
+			
+	
+ }
 }
